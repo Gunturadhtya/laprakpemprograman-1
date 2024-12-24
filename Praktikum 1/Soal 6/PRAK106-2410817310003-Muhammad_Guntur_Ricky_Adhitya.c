e4a74ed9ac9ff67e06425e35f769d3584101c0eb
Gunturadhtya <153581523+Gunturadhtya@users.noreply.github.com>
2024-12-24 09:33:54 +0800

    Add Modul 0

---
+++ Praktikum 1/Soal 6/PRAK106-2410817310003-Muhammad_Guntur_Ricky_Adhitya.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    int a = 4;
+    int b = 8;
+    int c = 3;
+    printf("Variabel a bernilai %d\n", a);
+    printf("Variabel b bernilai %d\n", b);
+    printf("Variabel c bernilai %d\n", c);
+    printf("apakah a sama dengan b? jawabannya adalah %d\n",(a==b));
+    printf("apakah b lebih besar dari c? jawabannya adalah %d\n",(b>c));
+    printf("apakah a tidak sama dengan b? jawabannya adalah %d\n",(a!=b));
+}
---
+++ Praktikum 1/Soal 6/PRAK106-2410817310003-Muhammad_Guntur_Ricky_Adhitya.py
@@ -0,0 +1,9 @@
+a = 4
+b = 8
+c = 3
+print("Variabel a bernilai %d"%a)
+print("Variabel b bernilai %d"%b)
+print("Variabel c bernilai %d"%c)
+print("apakah a sama dengan b? jawabannya adalah %d"%(a==b))
+print("apakah b lebih besar dari c? jawabannya adalah %d"%(b>c))
+print("apakah a tidak sama dengan b? jawabannya adalah %d"%(a!=b))
